ad5c18f85c5f3385bb128c7c96e92edc9be107fb
Yago Moreira <yagomoreira_dev@yahoo.com>
2026-08-15 20:29:21 -0300

    Criado endpoint de teste para buscar via id | Adc anotações do swagger para documentar a api com @ApiReponses() e @ApiResponse| Usando a classe Optional

--- src/main/java/com/cloud/yagodev/api_tarefa_curso_nelio_alves/controllers/TaskController.java
+++ src/main/java/com/cloud/yagodev/api_tarefa_curso_nelio_alves/controllers/TaskController.java
@@ -3,6 +3,8 @@ package com.cloud.yagodev.api_tarefa_curso_nelio_alves.controllers;
 import com.cloud.yagodev.api_tarefa_curso_nelio_alves.models.Task;
 import com.cloud.yagodev.api_tarefa_curso_nelio_alves.repositories.TaskRepository;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,9 +27,16 @@ public class TaskController {
         return "Teste de api bem sucedido!";
     }
 
+
     @GetMapping("/buscaId")
+    @Operation(summary = "Buscar tarefa por Id.", description = "Busque um tarefa informando o número de um id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tarefa encontrada com sucesso!"),
+    })
     public String pegarUmaTarefa(Long id) {
         Optional<Task> buscarValorDescription = taskRepository.findById(id);
+        //Task task = buscarValorDescription.get();
+        // return task.getDescription;
         return buscarValorDescription.get().getDescription();
     }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,14 +1,9 @@
-INSERT INTO tarefas (descricao, concluido)
-VALUES ('Comprar mantimentos', FALSE);
+INSERT INTO tb_tarefas (descricao, concluido) VALUES('Comprar mantimentos', FALSE);
 
-INSERT INTO tarefas (descricao, concluido)
-VALUES ('Jogar o lixo', FALSE);
+INSERT INTO tb_tarefas (descricao, concluido) VALUES('Jogar o lixo', FALSE);
 
-INSERT INTO tarefas (descricao, concluido)
-VALUES ('Banhar cachorro', FALSE);
+INSERT INTO tb_tarefas (descricao, concluido) VALUES('Banhar cachorro', FALSE);
 
-INSERT INTO tarefas (descricao, concluido)
-VALUES ('Lavar o carro', FALSE);
+INSERT INTO tb_tarefas (descricao, concluido) VALUES('Lavar o carro', FALSE);
 
-INSERT INTO tarefas (descricao, concluido)
-VALUES ('Tomar remédio', FALSE);
+INSERT INTO tb_tarefas (descricao, concluido) VALUES('Tomar remédio', FALSE);
