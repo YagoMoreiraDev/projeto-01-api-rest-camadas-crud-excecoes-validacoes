0cbcc3cb0a04eaec3d88eeb756f9c5d7c953b356
Yago Moreira <yagomoreira_dev@yahoo.com>
2026-08-15 18:02:49 -0300

    Iniciando o projeto-01(api de tarefas) API REST, camadas, CRUD, exceções, validações | Curso Nelio Alves | Configurado as variaveis de ambient e o .env

---
+++ src/main/java/com/cloud/yagodev/api_tarefa_curso_nelio_alves/ApiTarefaCursoNelioAlvesApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.yagodev.api_tarefa_curso_nelio_alves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTarefaCursoNelioAlvesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTarefaCursoNelioAlvesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloud/yagodev/api_tarefa_curso_nelio_alves/ApiTarefaCursoNelioAlvesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.yagodev.api_tarefa_curso_nelio_alves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTarefaCursoNelioAlvesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
