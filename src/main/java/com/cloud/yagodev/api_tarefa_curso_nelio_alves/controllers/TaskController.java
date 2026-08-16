4a718a95001231119a38c91699d8abd9b30bc983
Yago Moreira <yagomoreira_dev@yahoo.com>
2026-08-16 12:05:55 -0300

    Separando de forma correta as camadas de responsabilidades e usando os a classe Mappers_DTO | Controller -> DTOs -> Services -> Repositories

--- src/main/java/com/cloud/yagodev/api_tarefa_curso_nelio_alves/controllers/TaskController.java
+++ src/main/java/com/cloud/yagodev/api_tarefa_curso_nelio_alves/controllers/TaskController.java
@@ -1,11 +1,14 @@
 package com.cloud.yagodev.api_tarefa_curso_nelio_alves.controllers;
 
+import com.cloud.yagodev.api_tarefa_curso_nelio_alves.dtos.TaskDTO;
 import com.cloud.yagodev.api_tarefa_curso_nelio_alves.models.Task;
 import com.cloud.yagodev.api_tarefa_curso_nelio_alves.repositories.TaskRepository;
+import com.cloud.yagodev.api_tarefa_curso_nelio_alves.services.TaskService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +18,10 @@ import java.util.Optional;
 @RequestMapping(value = "/task")
 public class TaskController {
 
-    private TaskRepository taskRepository;
+    private TaskService taskService;
 
-    public TaskController(TaskRepository taskRepository) {
-        this.taskRepository = taskRepository;
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
     }
 
     @GetMapping("/teste")
@@ -28,15 +31,12 @@ public class TaskController {
     }
 
 
-    @GetMapping("/buscaId")
+    @GetMapping("/{id}") //O "/{id}" casa com o @PathVariable
     @Operation(summary = "Buscar tarefa por Id.", description = "Busque um tarefa informando o número de um id")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Tarefa encontrada com sucesso!"),
     })
-    public String pegarUmaTarefa(Long id) {
-        Optional<Task> buscarValorDescription = taskRepository.findById(id);
-        //Task task = buscarValorDescription.get();
-        // return task.getDescription;
-        return buscarValorDescription.get().getDescription();
+    public TaskDTO pegarUmaTarefa(@PathVariable Long id) {
+        return taskService.buscarTarefaPorId(id);
     }
 }
---
+++ src/main/java/com/cloud/yagodev/api_tarefa_curso_nelio_alves/services/TaskService.java
@@ -0,0 +1,29 @@
+package com.cloud.yagodev.api_tarefa_curso_nelio_alves.services;
+
+
+import com.cloud.yagodev.api_tarefa_curso_nelio_alves.dtos.TaskDTO;
+import com.cloud.yagodev.api_tarefa_curso_nelio_alves.dtos.mappers.TaskMapper;
+import com.cloud.yagodev.api_tarefa_curso_nelio_alves.models.Task;
+import com.cloud.yagodev.api_tarefa_curso_nelio_alves.repositories.TaskRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class TaskService {
+
+    private TaskRepository taskRepository;
+    private TaskMapper taskMapper;
+
+    public TaskService(TaskRepository taskRepository, TaskMapper taskMapper) {
+        this.taskRepository = taskRepository;
+        this.taskMapper = taskMapper;
+    }
+
+    @Transactional(readOnly = true)
+    public TaskDTO buscarTarefaPorId(Long id) {
+        Optional<Task> taskEntidade = taskRepository.findById(id);
+        return taskMapper.entidadeParaDto(taskEntidade.get());
+    }
+}
